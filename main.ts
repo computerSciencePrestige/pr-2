be37c0471939d20867f2cea4610e83ec54e1471f
computerSciencePrestige <37340060+computerSciencePrestige@users.noreply.github.com>
2021-02-26 18:24:18 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    counter += -1
-    if (counter < 0) {
-        counter = 10
-    }
-    basic.showNumber(counter)
-})
-input.onButtonPressed(Button.B, function () {
     counter += 1
     if (counter > 10) {
         counter = 0
     }
     basic.showNumber(counter)
 })
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+    if (counter < 0) {
+        counter = 10
+    }
+    basic.showNumber(counter)
+})
 let counter = 0
-counter = 0
+counter = 10
 basic.showNumber(counter)
 basic.forever(function () {
 	
