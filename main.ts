5f7103bda5fa6ab2366ab66c2be7f502d865fb22
computerSciencePrestige <37340060+computerSciencePrestige@users.noreply.github.com>
2021-02-26 19:33:15 -0500

    pr5 end

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let random_number = 0
 input.onButtonPressed(Button.A, function () {
-    random_number = randint(1, 6)
+    random_number = randint(1, 10)
     if (random_number == 1) {
         basic.showString("PE with  Joe")
     } else if (random_number == 2) {
@@ -11,7 +11,39 @@ input.onButtonPressed(Button.A, function () {
         basic.showString("Tidy our rooms")
     } else if (random_number == 5) {
         basic.showString("Play a card game")
+    } else if (random_number == 6) {
+        basic.showString("Drive a car")
+    } else if (random_number == 7) {
+        basic.showString("Go to a cafe")
+    } else if (random_number == 8) {
+        basic.showString("Watch TV")
+    } else if (random_number == 9) {
+        basic.showString("Take a walk")
     } else {
-        basic.showString("Learn a song")
+        basic.showString("Read a book")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    random_number = randint(1, 8)
+    if (random_number == 1) {
+        basic.showString("PE with  Joe")
+    } else if (random_number == 2) {
+        basic.showString("Watch a Movie")
+    } else if (random_number == 3) {
+        basic.showString("Play a Board Game")
+    } else if (random_number == 4) {
+        basic.showString("Tidy our rooms")
+    } else if (random_number == 5) {
+        basic.showString("Play a card game")
+    } else if (random_number == 6) {
+        basic.showString("Drive a car")
+    } else if (random_number == 7) {
+        basic.showString("Go to a cafe")
+    } else if (random_number == 8) {
+        basic.showString("Watch TV")
+    } else if (random_number == 9) {
+        basic.showString("Take a walk")
+    } else {
+        basic.showString("Read a book")
     }
 })
