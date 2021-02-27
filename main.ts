3489bbe87fc6c4a9c7bb3e211e679df75c975f0b
computerSciencePrestige <37340060+computerSciencePrestige@users.noreply.github.com>
2021-02-26 19:15:29 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    amount = 1
-})
-input.onButtonPressed(Button.B, function () {
-    amount = -1
-})
-input.onGesture(Gesture.Shake, function () {
-    steps += amount
-    basic.showNumber(steps)
-})
-let amount = 0
-let steps = 0
-steps = 0
-amount = 1
+
