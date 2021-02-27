e27ea8e96e4e4af647083407090881830562b0de
computerSciencePrestige <37340060+computerSciencePrestige@users.noreply.github.com>
2021-02-26 19:48:30 -0500

    pr 7

--- main.ts
+++ main.ts
@@ -1,49 +1,15 @@
-let random_number = 0
-input.onButtonPressed(Button.A, function () {
-    random_number = randint(1, 10)
-    if (random_number == 1) {
-        basic.showString("PE with  Joe")
-    } else if (random_number == 2) {
-        basic.showString("Watch a Movie")
-    } else if (random_number == 3) {
-        basic.showString("Play a Board Game")
-    } else if (random_number == 4) {
-        basic.showString("Tidy our rooms")
-    } else if (random_number == 5) {
-        basic.showString("Play a card game")
-    } else if (random_number == 6) {
-        basic.showString("Drive a car")
-    } else if (random_number == 7) {
-        basic.showString("Go to a cafe")
-    } else if (random_number == 8) {
-        basic.showString("Watch TV")
-    } else if (random_number == 9) {
-        basic.showString("Take a walk")
-    } else {
-        basic.showString("Read a book")
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Skull)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.pause(100)
     }
-})
-input.onButtonPressed(Button.B, function () {
-    random_number = randint(1, 8)
-    if (random_number == 1) {
-        basic.showString("PE with  Joe")
-    } else if (random_number == 2) {
-        basic.showString("Watch a Movie")
-    } else if (random_number == 3) {
-        basic.showString("Play a Board Game")
-    } else if (random_number == 4) {
-        basic.showString("Tidy our rooms")
-    } else if (random_number == 5) {
-        basic.showString("Play a card game")
-    } else if (random_number == 6) {
-        basic.showString("Drive a car")
-    } else if (random_number == 7) {
-        basic.showString("Go to a cafe")
-    } else if (random_number == 8) {
-        basic.showString("Watch TV")
-    } else if (random_number == 9) {
-        basic.showString("Take a walk")
-    } else {
-        basic.showString("Read a book")
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.House)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.pause(100)
     }
+    basic.clearScreen()
 })
